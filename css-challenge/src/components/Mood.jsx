0137ea85cfe6e22062b33dd592e7912c787e5ad3
Petra <petra.jakobcic@gmail.com>
2021-07-25 08:57:14 +0100

    Create the Mood component and outline the content

---
+++ css-challenge/src/components/Mood.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default function Mood() {
+	return (
+		<section>
+			<div>
+				<div>How do you feel?</div>
+
+				<div>Two dots</div>
+			</div>
+
+			<div>
+				<div>
+					<div>Emoji</div>
+					<span>Bad</span>
+				</div>
+				<div>
+
+					<div>Emoji</div>
+					<span>Fine</span>
+				</div>
+
+				<div>
+					<div>Emoji</div>
+					<span>Well</span>
+				</div>
+
+				<div>
+					<div>Emoji</div>
+					<span>Excellent</span>
+				</div>
+			</div>
+		</section>
+	)
+}
