d38ad34eba5952aeba483f0b43909ab25c8025af
Petra <petra.jakobcic@gmail.com>
2021-07-25 09:18:05 +0100

    Import the newly created components to the App.js file

--- css-challenge/src/App.js
+++ css-challenge/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import Mood from './components/Mood';
+import Exercises from './components/Exercises';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <div className="Container">CODE GOES HERE</div>
+      <div className="container">
+        <Header />
+
+        <SearchBar />
+
+        <Mood />
+
+        <Exercises />
+
+        <NavBar />
+      </div>
     </div>
   );
 }
