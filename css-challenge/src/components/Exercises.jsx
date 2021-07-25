22cc6cb4689f1be883ca2a15026188df0421334d
Petra <petra.jakobcic@gmail.com>
2021-07-25 09:11:30 +0100

    Create the Exercises and Exercise components and outline the content

---
+++ css-challenge/src/components/Exercises.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Exercise from './Exercise';
+
+export default function Exercises() {
+	return (
+		<section>
+			<div>
+				<div>Exercises</div>
+
+				<div>Two dots</div>
+			</div>
+
+			<Exercise />
+
+			<Exercise />
+		</section>
+	)
+}
