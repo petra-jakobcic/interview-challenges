3e724cdd53441b002a63915e82aec507fd3507bd
Petra <petra.jakobcic@gmail.com>
2021-07-25 08:31:59 +0100

    Create the Header component and outline the content

---
+++ css-challenge/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header() {
+	return (
+		<header>
+			<div>
+				<div>Hi, Jared!</div>
+
+				<div>23 Jan, 2021</div>
+			</div>
+
+			<div>Notification bell icon</div>
+		</header>
+	)
+}
