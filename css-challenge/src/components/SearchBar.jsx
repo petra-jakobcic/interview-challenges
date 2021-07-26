0915f44baa4ba9c2ac945e930e7fa8db46ddb05c
Petra <petra.jakobcic@gmail.com>
2021-07-26 14:15:32 +0100

    Style the search bar

--- css-challenge/src/components/SearchBar.jsx
+++ css-challenge/src/components/SearchBar.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 export default function SearchBar() {
 	return (
-		<section>
-			<div>Search icon</div>
+		<section className="searchbar u-margin-bottom-medium">
+			<div className="searchbar__icon-container">
+				<i className="searchbar__icon fas fa-search"></i>
+			</div>
 
 			<form>
-				<input type="text" placeholder="Search" />
+				<input className="searchbar__input" type="text" placeholder="Search" />
 			</form>
 		</section>
 	)
