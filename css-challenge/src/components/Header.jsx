262fe734a1ae3a5aed199407ada7a41a389b1f23
Petra <petra.jakobcic@gmail.com>
2021-07-26 12:05:33 +0100

    Style the header

--- css-challenge/src/components/Header.jsx
+++ css-challenge/src/components/Header.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 export default function Header() {
 	return (
-		<header>
+		<header className="header u-margin-bottom-medium">
 			<div>
-				<div>Hi, Jared!</div>
+				<div className="header__greeting u-margin-bottom-small">Hi, Jared!</div>
 
-				<div>23 Jan, 2021</div>
+				<div className="header__date">23 Jan, 2021</div>
 			</div>
 
-			<div>Notification bell icon</div>
+			<div>
+				<i className="header__icon fas fa-bell"></i>
+			</div>
 		</header>
 	)
 }
